d9f8d371263a826d8da1a2b372a0131af8b0f140
vercatalano1 <ver.catalano1@stud.uniroma3.it>
2024-05-14 10:09:27 +0200

    inizio

---
+++ siw-food-1/src/test/java/it/uniroma3/siw/SiwFood1ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwFood1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
