5f759c3c40159c8a7ed9f2fddf96adbe3b9d5b10
vercatalano1 <ver.catalano1@stud.uniroma3.it>
2024-06-18 15:50:31 +0200

    caricamento immagini delle ricette

--- siw-food-1/src/main/resources/import.sql
+++ siw-food-1/src/main/resources/import.sql
@@ -1,19 +1,24 @@
-insert into cuoco (id, nome, cognome, data, email) VALUES (nextval('cuoco_seq'), 'Max', 'Mariola', '11-04-1969', 'maxmariola@gmail.com');
-insert into cuoco (id, nome, cognome, data, email) values(nextval('cuoco_seq'), 'Carlo', 'Cracco', '08-10-1965', ' carlocracco@gmial.com');
-insert into cuoco (id, nome, cognome, data, email) values(nextval('cuoco_seq'), 'Antonia', 'Klugmann', '02-09-1979', 'antoniaklugmann@gmail.com');
-insert into cuoco (id, nome, cognome, data, email) values(nextval('cuoco_seq'), 'Enrico', 'Bartolini', '24-11-1979', 'enricobartolini@gmail.com');
-insert into cuoco (id, nome, cognome, data, email) values(nextval('cuoco_seq'), 'Bruno', 'Barbieri', '12-01-1962', 'brunobarbieri@gmail.com');
-insert into cuoco (id, nome, cognome, data, email) values(nextval('cuoco_seq'), 'Nadia', 'Santini', '19-07-1964', 'nadiasantini@gmail.com');
-insert into cuoco (id, nome, cognome, data, email) values(nextval('cuoco_seq'), 'Alex', 'Atala', '03-06-1968', 'alexatala@gmail.com');
+insert into cuoco (id, nome, cognome, data, email, image ) VALUES (nextval('cuoco_seq'), 'Max', 'Mariola', '11-04-1969', 'maxmariola@gmail.com', 'mariola.png');
+insert into cuoco (id, nome, cognome, data, email, image ) values(nextval('cuoco_seq'), 'Carlo', 'Cracco', '08-10-1965', ' carlocracco@gmial.com','cracco.png' );
+insert into cuoco (id, nome, cognome, data, email, image ) values(nextval('cuoco_seq'), 'Antonia', 'Klugmann', '02-09-1979', 'antoniaklugmann@gmail.com','klugmann.png');
+insert into cuoco (id, nome, cognome, data, email, image ) values(nextval('cuoco_seq'), 'Enrico', 'Bartolini', '24-11-1979', 'enricobartolini@gmail.com','bartolini.png');
+insert into cuoco (id, nome, cognome, data, email, image ) values(nextval('cuoco_seq'), 'Bruno', 'Barbieri', '12-01-1962', 'brunobarbieri@gmail.com','barbieri.png');
+insert into cuoco (id, nome, cognome, data, email, image ) values(nextval('cuoco_seq'), 'Nadia', 'Santini', '19-07-1964', 'nadiasantini@gmail.com','santini.png');
 
-insert into ricetta (id, nome, descrizione, cuoco_id) VALUES (nextval('ricetta_seq'), 'Zuppa di pomodorini con stracciatella e alici', 'Un’originale zuppa di pomodorini fresca arricchita con stracciatella e alici e insalata di cipolle in agrodolce che danno al piatto un sapore che non ti aspetti. Una zuppa diversa dal solito, che stuzzica le papille gustative attraverso sapori decisi e al tempo stesso freschi.', 1);
-insert into ricetta (id, nome, descrizione, cuoco_id) values(nextval('ricetta_seq'), 'Risotto con acciughe, limone e cacao', 'Un piatto sorprendente che unisce sapori audaci: il risotto cremoso con acciughe sott’olio, limone fresco e un tocco di cacao puro. La pasta di acciughe dona un sapore intenso, mentre la scorza di limone aggiunge freschezza. Il cacao crea un contrasto inaspettato, rendendo questo piatto un’esperienza gustativa unica.', 51);
-insert into ricetta (id, nome, descrizione, cuoco_id) values(nextval('ricetta_seq'), 'Guancia di maiale brasata alle susine', 'Un piatto succulento e ricco, la guancia di maiale brasata alle susine è un’esplosione di sapori. La carne di maiale, lentamente cotta nel vino Ramandolo, diventa tenera e avvolgente. Le susine aggiungono una dolcezza naturale, mentre le erbe aromatiche completano il piatto.', 101);
-insert into ricetta (id, nome, descrizione, cuoco_id) values(nextval('ricetta_seq'), 'Cannolo di zafferano con formaggio di pecora e gremolada', 'Un connubio sorprendente tra ingredienti classici e creatività.: il cannolo di zafferano, farcito con formaggio di pecora d’alpeggio, è un’esplosione di sapori. La pasta sottile e croccante avvolge il formaggio cremoso, mentre la salsa gremolada, con aglio, prezzemolo e buccia di limone, completa l’esperienza gustativa.', 151);
-insert into ricetta (id, nome, descrizione, cuoco_id) values(nextval('ricetta_seq'), 'Lasagnetta al gorgonzola dolce', 'La lasagnetta al gorgonzola dolce di Bruno Barbieri è come una sinfonia di sapori. La pasta sfoglia, sottile e delicata, avvolge il cuore cremoso del gorgonzola, che si scioglie lentamente al calore del forno. Ogni strato di pasta è un abbraccio di comfort, mentre il cavolfiore, tenero e leggermente croccante, aggiunge una nota di freschezza.', 201);
-insert into ricetta (id, nome, descrizione, cuoco_id) values(nextval('ricetta_seq'), 'Terrina di astice con caviale Oscietra e zenzero marinato', 'Questa terrina è un inno alla tradizione e all’innovazione, un piatto che racchiude l’amore per gli ingredienti di alta qualità e la maestria culinaria della famiglia Santini. una terrina elegante e raffinata, composta da strati di astice fresco e caviale Oscietra Royal, il quale aggiunge una nota di lusso e intensità al piatto. L’astice, delicatamente marinato, si fonde con il caviale, creando un equilibrio tra sapori di mare e note terrose.', 251);
-insert into ricetta (id, nome, descrizione, cuoco_id) values(nextval('ricetta_seq'), 'Capesante con latte di cocco, pepe aromatico e mango croccante', 'Il piatto è un equilibrio tra freschezza, calore e croccantezza. Capesante succulente e tenere avvolte in una veste cremosa e delicata, con una nota esotica e tropicale.Infine un tocco di pepe nero o rosa che aggiunge profondità e un leggero pizzico di piccantezza', 301);
-
+insert into ricetta (id, nome, descrizione, image, cuoco_id) VALUES (nextval('ricetta_seq'), 'Zuppa di pomodorini con stracciatella e alici', 'Un’originale zuppa di pomodorini fresca arricchita con stracciatella e alici e insalata di cipolle in agrodolce che danno al piatto un sapore che non ti aspetti. Una zuppa diversa dal solito, che stuzzica le papille gustative attraverso sapori decisi e al tempo stesso freschi.', 'zuppa.png', 1);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Risotto con acciughe, limone e cacao', 'Un piatto sorprendente che unisce sapori audaci: il risotto cremoso con acciughe sott’olio, limone fresco e un tocco di cacao puro. La pasta di acciughe dona un sapore intenso, mentre la scorza di limone aggiunge freschezza. Il cacao crea un contrasto inaspettato, rendendo questo piatto un’esperienza gustativa unica.','risotto.png', 51);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Guancia di maiale brasata alle susine', 'Un piatto succulento e ricco, la guancia di maiale brasata alle susine è un’esplosione di sapori. La carne di maiale, lentamente cotta nel vino Ramandolo, diventa tenera e avvolgente. Le susine aggiungono una dolcezza naturale, mentre le erbe aromatiche completano il piatto.','guancia.png', 101);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Cannolo di zafferano con formaggio di pecora e gremolada', 'Un connubio sorprendente tra ingredienti classici e creatività.: il cannolo di zafferano, farcito con formaggio di pecora d’alpeggio, è un’esplosione di sapori. La pasta sottile e croccante avvolge il formaggio cremoso, mentre la salsa gremolada, con aglio, prezzemolo e buccia di limone, completa l’esperienza gustativa.','cannolo.png', 151);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Lasagnetta al gorgonzola dolce', 'La lasagnetta al gorgonzola dolce di Bruno Barbieri è come una sinfonia di sapori. La pasta sfoglia, sottile e delicata, avvolge il cuore cremoso del gorgonzola, che si scioglie lentamente al calore del forno. Ogni strato di pasta è un abbraccio di comfort, mentre il cavolfiore, tenero e leggermente croccante, aggiunge una nota di freschezza.','lasagnetta.png', 201);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Terrina di astice con caviale Oscietra e zenzero marinato', 'Questa terrina è un inno alla tradizione e all’innovazione, un piatto che racchiude l’amore per gli ingredienti di alta qualità e la maestria culinaria della famiglia Santini. una terrina elegante e raffinata, composta da strati di astice fresco e caviale Oscietra Royal, il quale aggiunge una nota di lusso e intensità al piatto. L’astice, delicatamente marinato, si fonde con il caviale, creando un equilibrio tra sapori di mare e note terrose.', 'terrina.png', 251);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) VALUES (nextval('ricetta_seq'), 'Tartare cotta con burrata e alici', 'La tartare cotta con burrata e alici è un piatto che unisce sapori intensi e contrastanti in modo sorprendente. Preparata con cura e maestria, questa ricetta offre un''esperienza culinaria unica che conquista per la sua semplicità e raffinatezza.', 'tartare.png', 1);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Sfoglie di polenta croccante', 'Le sfoglie di polenta croccante sono un delizioso antipasto che unisce la consistenza croccante della polenta fritta con un cuore morbido e saporito.','polenta.png', 51);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Tuorlo marinato con fonduta', 'Il tuorlo marinato con fonduta è un piatto sofisticato che unisce la delicatezza del tuorlo d''uovo marinato alla cremosità della fonduta. Questo piatto crea un equilibrio perfetto tra la morbidezza dell''uovo e la cremosità del formaggio, regalando un''esperienza gastronomica raffinata e memorabile.','tuorlo.png', 51);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Spaghetti ai frutti rossi e acciughe', 'Gli spaghetti ai frutti rossi e acciughe rappresentano un connubio audace tra il dolce dei frutti rossi e il sapore salato delle acciughe, creando un piatto intrigante e gustoso. Le acciughe aggiungono profondità e un tocco salino alla salsa, mentre i frutti rossi, con la loro dolcezza naturale e un lieve accento acidulo, bilanciano il piatto.','spaghetti.png', 101);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Gnocchi di albicocche', 'Gli gnocchi di albicocche sono una deliziosa variante dei tradizionali gnocchi, preparati con albicocche mature e farina. Questo piatto offre un mix unico di dolcezza fruttata e consistenza delicata','gnocchi.png', 101);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Bloody beetroot', 'Questo piatto si distingue per l''elegante presentazione e per l''equilibrio tra i sapori dolci e terrosi della barbabietola, spesso accostata a ingredienti complementari che esaltano la sua naturale dolcezza ','bloody.png', 151);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Cartoccio di riso', 'Questo piatto non è solo un piatto da gustare, ma un''esperienza culinaria che mescola sapori autentici con la creatività e l''attenzione ai dettagli. È un invito a immergersi in un viaggio gustativo di lusso e innovazione, dove ogni morso rivela un nuovo strato di sapore e piacere.','riso.png', 151);
+insert into ricetta (id, nome, descrizione, image, cuoco_id) values(nextval('ricetta_seq'), 'Risotto con tarassaco e gorgonzola dolce', 'Il risotto con tarassaco e gorgonzola dolce è una fusione audace di sapori contrastanti, che eleva la semplicità degli ingredienti a un livello di raffinatezza.','risotto2.png', 201);
 
 
 insert into ingrediente (id, nome, quantità) VALUES (nextval('ingrediente_seq'),'Pomodorini ', '600');
@@ -66,14 +71,6 @@ insert into ingrediente (id, nome, quantità) values(nextval('ingrediente_seq'),
 insert into ingrediente (id, nome, quantità) values(nextval('ingrediente_seq'), 'Caviale Oscietra', '75');
 insert into ingrediente (id, nome, quantità) values(nextval('ingrediente_seq'), '1-2 radici di zenzero marinato', null);
 insert into ingrediente (id, nome, quantità) values(nextval('ingrediente_seq'), 'succo di limone', null);
-
-insert into ingrediente (id, nome, quantità) values(nextval('ingrediente_seq'), '8 pezzi di capesante fresche', null);
-insert into ingrediente (id, nome, quantità) values(nextval('ingrediente_seq'), '400 ml di latte di cocco', null);
-insert into ingrediente (id, nome, quantità) values(nextval('ingrediente_seq'), 'pepe rosa', null);
-insert into ingrediente (id, nome, quantità) values(nextval('ingrediente_seq'), '1 mango maturo', null);
-insert into ingrediente (id, nome, quantità) values(nextval('ingrediente_seq'), 'olio vegetale', null);
-insert into ingrediente (id, nome, quantità) values(nextval('ingrediente_seq'), '2 cucchiai di zucchero di canna', null);
-
 
 
 INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (1, 1);
@@ -150,15 +147,7 @@ INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (1351, 251);
 INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (1601, 251);
 
 
-INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (2251, 301);
-INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (2301, 301);
-INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (2351, 301);
-INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (2401, 301);
-INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (2451, 301);
-INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (2501, 301);
-INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (301, 301);
-INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (1601, 301);
-INSERT INTO ricetta_ingredienti (ingredienti_id, ricette_id) VALUES (1401, 301);
+
 
 
 
